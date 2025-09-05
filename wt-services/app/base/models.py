3111b6e800fa36c9b0ec01af1675d140759f448f
asael3301 <asaegalv14@gmail.com>
2025-09-05 00:32:41 -0400

    added the base app for the backend

---
+++ wt-services/app/base/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+# data models will be defined here
---
+++ wt-services/app/base/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+
+# functions that handle requests and return responses will be defined here
