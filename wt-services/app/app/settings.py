58468f502dd1abb025f625ff73a7e6d1d24b5593
asael3301 <asaegalv14@gmail.com>
2025-12-04 18:14:45 -0500

    vercel origins

--- wt-services/app/app/settings.py
+++ wt-services/app/app/settings.py
@@ -62,6 +62,8 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://*.vercel.app",
+    "https://*.onrender.com",
  ]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
  
 MIDDLEWARE = [
